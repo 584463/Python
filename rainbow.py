f24fadc115b5fd5442c0613d1b2a2363c4a06844
mstr3sl <145142792+584463@users.noreply.github.com>
2023-09-27 11:24:30 -0400

    Add files via upload

---
+++ rainbow.py
@@ -0,0 +1,43 @@
+import time, sys
+import bext
+
+print("Rainbow Time!")
+print("Press Ctrl-C to stop.")
+
+time.sleep(3)
+
+indent = 0
+indentIncreasing = True
+
+while True:
+    print(" " * indent, end="")
+
+    bext.fg("red")
+    print("##", end="")
+
+    bext.fg("yellow")
+    print("##", end="")
+
+    bext.fg("green")
+    print("##", end="")
+
+    bext.fg("blue")
+    print("##", end="")
+
+    bext.fg("cyan")
+    print("##", end="")
+
+    bext.fg("purple")
+    print("##")
+
+    if indentIncreasing:
+        indent += 1
+    else:
+        indent-=1
+    
+    
+    if indent == 100:
+        indentIncreasing = False
+        if indent == 0:
+            indentIncreasing = True
+    time.sleep(0.1)
